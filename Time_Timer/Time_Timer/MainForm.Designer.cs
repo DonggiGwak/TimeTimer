7c83f9b8af668576c27cc0db182582572389ffd3
dggwak88@gmail.com <dggwak88@gmail.com>
2019-03-10 19:22:18 +0900

    Time Timer
    TimeTimer

---
+++ Time_Timer/Time_Timer/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Time_Timer
+{
+    partial class Time_Timer
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Time_Timer));
+            this.pic_clock = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_clock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_clock
+            // 
+            this.pic_clock.Image = global::Time_Timer.Properties.Resources.시계;
+            this.pic_clock.Location = new System.Drawing.Point(12, 12);
+            this.pic_clock.Name = "pic_clock";
+            this.pic_clock.Size = new System.Drawing.Size(300, 300);
+            this.pic_clock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_clock.TabIndex = 0;
+            this.pic_clock.TabStop = false;
+            this.pic_clock.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_clock_MouseMove);
+            this.pic_clock.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_clock_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 40F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(16, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0 : 00 : 00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Time_Timer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 374);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic_clock);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Time_Timer";
+            this.Text = "Time Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_clock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_clock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Time_Timer/Time_Timer/MainForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Time_Timer
+{
+    public partial class Time_Timer : Form
+    {
+        PictureBox pic_origin = new PictureBox();
+        int leftTime = 0;
+
+        public Time_Timer()
+        {
+            InitializeComponent();
+
+            pic_origin.Image = Properties.Resources.시계;
+            pic_origin.SizeMode = PictureBoxSizeMode.StretchImage;
+            TopMost = true;
+        }
+
+        private void pic_clock_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                double x1 = 0, y1 = pic_clock.Width;
+                double x2 = -(pic_clock.Height / 2 - e.Y), y2 = e.X + pic_clock.Width / 2;
+
+                double Degree = 180 * (Math.Atan2(y2 - y1, x2 - x1) / Math.PI + 1);
+                leftTime = (int)Degree * 10;    //Second
+
+                pic_clock.Refresh();
+                Graphics graphics = pic_clock.CreateGraphics();
+                SolidBrush solidBrush = new SolidBrush(Color.Red);
+                graphics.FillPie(solidBrush, new Rectangle(4, 4, 292, 292), -90, -(float)Degree);
+
+                int nHour = Convert.ToInt32(Math.Truncate(leftTime / 3600.0));
+                int nMin = Convert.ToInt32(Math.Truncate(leftTime / 60.0));
+                if (nMin == 60)
+                    nMin = 0;
+                int nSec = Convert.ToInt32(leftTime % 60);
+                label1.Text = nHour + " : " + nMin + " : " + nSec;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Time Over
+            if (leftTime == 0)
+            {
+                timer1.Enabled = false;
+                //Alarm
+                SoundPlayer soundPlayer = new SoundPlayer(Application.StartupPath + "\\Alarm.wav");
+                soundPlayer.Play();
+                return;
+            }
+            else
+            {
+                leftTime -= 1;
+
+                pic_clock.Refresh();
+                Graphics graphics = pic_clock.CreateGraphics();
+                SolidBrush solidBrush = new SolidBrush(Color.Red);
+                graphics.FillPie(solidBrush, new Rectangle(4, 4, 292, 292), -90, -(leftTime / 10));
+
+                int nHour = Convert.ToInt32(Math.Truncate(leftTime / 3600.0));
+                int nMin = Convert.ToInt32(Math.Truncate(leftTime / 60.0));
+                if (nMin == 60)
+                    nMin = 0;
+                int nSec = Convert.ToInt32(leftTime % 60);
+                label1.Text = nHour + " : " + nMin + " : " + nSec;
+            }
+        }
+
+        private void pic_clock_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
